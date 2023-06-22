ac95c22ce7103cee2f845d86919a13d35c17ca0c
PasinduO <mailbox.pasindu@gmail.com>
2023-06-22 09:51:23 +0530

    home page finalized

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,177 +1,4 @@
-// swiper js
-var curtains_slider_settings = {
-  speed: 400,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.curtains-pagination',
-    type: 'bullets',
-    clickable: true 
-  },
-    autoplay: {
-    delay: 2000,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    }
-  }
-}
-
-var curtains_slider = new Swiper('.curtains-slider', curtains_slider_settings)
-
-const blinds_slider_settings = {
-  speed: 600,
-  spaceBetween: 10,
-  slidesPerView: 2,
-  loop: true,
-  pagination: {
-    el: '.blinds-pagination',
-    type: 'bullets',
-  },
-    autoplay: {
-    delay: 2000,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    }
-  }
-}
-
-// const blinds_slider = new Swiper('.blinds-slider', blinds_slider_settings);
-// const swiper = new Swiper('.swiper', {
-//   speed: 400,
-//   spaceBetween: 15,
-//   slidesPerView: 2,
-//   loop: true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     type: 'bullets',
-//   },
-//   navigation: {
-//     nextEl: null,
-//     prevEl: null,
-//   },
-//   autoplay: {
-//     delay: 2000,
-//   },
-//   breakpoints:{
-//     200: {
-//       slidesPerView: 2,
-//       spaceBetween: 20,
-//       nextEl: null,
-//       prevEl: null,
-//     },
-//     1024: {
-//       slidesPerView: 4,
-//       spaceBetween: 40,
-//       nextEl: null,
-//       prevEl: null,
-//     }
-//   }
-// });
-
-// const prevBtn = document.querySelector('.swiper-button-prev')
-
-// prevBtn.addEventListener('click', function () { 
-//   swiper.slidePrev(500)
-//  })
-
-// testimonial slider
-const testimonial_slider_settings = {
-  speed: 400,
-  spaceBetween: 0,
-  slidesPerView: 1,
-  loop: true,
-  // centeredSlides: true,
-  // pagination: {
-  //   el: '.testimonial-pagination',
-  //   type: 'bullets',
-  //   clickable: true
-  // },
-  // navigation: {
-  //   nextEl: '.testimonial-next',
-  //   prevEl: '.testimonial-prev',
-  // },
-  // autoplay: false
-  // autoplay: {
-  //   delay: 2000,
-  //   // enabled: false
-  // },
-}
-
-const testimonial_slider = new Swiper('.testimonial-slider', testimonial_slider_settings);
-
-
-// logo slider
-// note: Because of nature of how the loop mode works (it will rearrange slides), 
-//total number of slides must be >= slidesPerView * 2
-const logo_slider_settings = {
-  slidesPerView: 2,
-  spaceBetween: 20,
-  loop: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-    1140: {
-      slidesPerView: 6,
-    }
-  }
-}
-
-const logo_slider = new Swiper('.logo_slider', logo_slider_settings);
-
-
-// product gallery
-const product_slider_settings = {
-  slidesPerView: 2,
-  slidesPerGroup: 2,
-  spaceBetween: 10,
-  loop: true,
-  // rewind: true,
-  pagination: {
-    el: '.gallery-pagination',
-    type: 'bullets',
-    clickable: true
-  },
-  navigation: {
-    nextEl: '.gallery-next',
-    prevEl: '.gallery-prev',
-  },
-  autoplay: {
-    delay: 2000,
-  },
-}
-const product_slider = new Swiper('.product-gallery', product_slider_settings)
-
-
-
-// add a class on scroll
+// add class to navbar on scroll
 function addClassOnScroll(Element, scrollAmount, classToBeadded) {
   const element = document.getElementById(Element),
     offset = scrollAmount,
@@ -204,5 +31,79 @@ menuToggle.addEventListener('change', function () {
   }
 
 })
+
+$(document).ready(function () {
+  var carousel_slider_settings = {
+    autoplay: true,
+    loop: true,
+    autoplayTimeout: 4000,
+    margin: 10,
+    dots: true,
+    dotsEach: true,
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 2
+      },
+      769: {
+        items: 3
+      },
+      1024: {
+        items: 4,
+        nav: true
+      }
+    }
+
+  }
+
+  $(".curtains-slider").owlCarousel(carousel_slider_settings);        // curtains slider
+  $(".blinds-slider").owlCarousel(carousel_slider_settings);          // blind slider
+
+
+  // testimonial slider settings
+  var testimonial_slider_settings = {
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    margin: 10,
+    dots: true,
+    dotsEach: true,
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    items: 1,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  }
+  $(".testimonial-slider").owlCarousel(testimonial_slider_settings);
+
+  // logo slider
+  var logo_slider_settings = {
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplaySpeed: 6000,
+    dots: false,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 2
+      },
+      480: {
+        items: 3
+      },
+      768: {
+        items: 4
+      },
+      1024: {
+        items: 6
+      }
+    }
+  }
+
+  $('.logo-slider').owlCarousel(logo_slider_settings)
+});
 
 
